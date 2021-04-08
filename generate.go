e635ebd874e27ddc5d708c56eb973b66ca948c53
Bart Smykla <bartek@smykla.com>
2021-04-08 10:31:55 +0200

    fix(*) generate config proto in the same directory
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- generate.go
+++ generate.go
@@ -1,5 +1,3 @@
-//go:generate rm -f ./proto/generated/*
-//go:generate protoc --proto_path=./proto --go_opt=paths=source_relative --go_out=./proto/generated ./proto/config.proto
-//go:generate go build -o /tmp/kumadoc .
-//go:generate protoc -I=./tmp -I=. --plugin=protoc-gen-kumadoc=/tmp/kumadoc --kumadoc_out=./tmp/generated ./tmp/foo.proto
+//go:generate rm -f ./proto/*.pb.go
+//go:generate protoc --proto_path=./proto --go_opt=paths=source_relative --go_out=./proto ./proto/config.proto
 package main
